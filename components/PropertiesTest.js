f5d216b00ce8b061e9f686fa0510cec2aae33865
Jo2101 <jovana@darkleap.dev>
2024-09-02 14:15:43 +0200

    Fourth section | Properties details from Contentful

--- components/PropertiesTest.js
+++ components/PropertiesTest.js
@@ -1,59 +1,70 @@
+"use client";
+
 import styles from "./PropertiesTest.module.scss";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 // Contentful
 import { getEntries } from "@/lib/contentful";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"; //added in contentful Rich text field, got an error in the meantime
 
-const Properties = async () => {
-  const propertyDetails = [];
+const Properties = () => {
+  const [properties, setProperties] = useState([]); //ovdje cemo smjestiti sve podatke o nekretninama, prvo je prazan niz
+  const [activeTab, setActiveTab] = useState("Sales"); //nju ubacujemo da bi se prvo prikazala Sales kartica
 
-  const properties = await getEntries("listings");
-  console.log(properties);
-  console.log(properties.items[0].fields.address);
+  useEffect(() => {
+    //preko useEffecta uzimam podatke sa Contenfula
+    const propertiesData = async () => {
+      //asinhrona funckija, povlaci podatke u pozadini
+      const entries = await getEntries("listings");
+      setProperties(entries.items);
+    };
+    propertiesData();
+  }, []);
 
-  /*  for (let i = 0; i < properties.items.length; i++) {
-    let property = properties.items[i];
+  const tabClick = (tab) => {
+    //za promjenu aktivne kartice
+    setActiveTab(tab);
+  };
 
-    let propertyDetail = (
-      <div className={styles.property} key={i}>
-        <h3 className={styles.headline}>{`${property.fields.address}`}</h3>
-        <p className={styles.paragraph}>{`${property.fields.neighbourhood}`}</p>
-        <p className={styles.paragraph}>{`${property.fields.propertyType}`}</p>
-
-        <p className={styles.paragraph}>
-          <strong>
-            {property.fields.propertyType === "Rental"
-              ? "Monthly Rent:"
-              : "Asking Price:"}{" "}
-            {property.fields.price}
-          </strong>
-        </p>
-      </div>
-    );
-    propertyDetails[i] = propertyDetail;
-  } */
+  //kreiram novu listu properties, prolazi kroz sve nekretnine i filtriram one koji odgovaraju aktivnoj kartici
+  const showProperties = properties.filter(
+    (item) => item.fields.propertyCategory === activeTab
+  );
 
   return (
     <div className={styles.mainContainer}>
+      {/* Tab Navigation */}
+      <div className={styles.tabNavigation}>
+        <button
+          className={`${styles.tabButton} ${
+            activeTab === "Sales" ? styles.active : ""
+          }`}
+          onClick={() => tabClick("Sales")}
+        >
+          Sales
+        </button>
+        <button
+          className={`${styles.tabButton} ${
+            activeTab === "Rentals" ? styles.active : ""
+          }`}
+          onClick={() => tabClick("Rentals")}
+        >
+          Rentals
+        </button>
+      </div>
+
+      {/* Properties */}
       <div className={styles.properties}>
-        {properties.items
-          .filter((item) => item.fields.propertyCategory == "Rentals")
-          .map((property) => (
-            <div className={styles.property} key={property.sys.id}>
-              <Link href={`/properties/${property.sys.id}`}>
-                {" "}
-                <h3
-                  className={styles.headline}
-                >{`${property.fields.address}`}</h3>
-              </Link>
-              <p
-                className={styles.paragraph}
-              >{`${property.fields.neighbourhood}`}</p>
-              <p className={styles.paragraph}>
-                {property.fields.gallery &&
-                Array.isArray(property.fields.gallery) ? (
-                  property.fields.gallery.map((galleryItem, index) => (
+        {showProperties.map((property) => (
+          <div className={styles.property} key={property.sys.id}>
+            <p className={styles.paragraph}>
+              {property.fields.gallery &&
+              Array.isArray(property.fields.gallery) ? ( //provjerava da li postoji property.fields.gallery i da li je to niz
+                property.fields.gallery.map(
+                  (
+                    galleryItem,
+                    index //prikazivanje svih slika
+                  ) => (
                     <div key={index}>
                       <img
                         className={styles.galleryImage}
@@ -61,16 +72,22 @@ const Properties = async () => {
                         alt={`Gallery Image ${index}`}
                       />
                     </div>
-                  ))
-                ) : (
-                  <p>No gallery available</p>
-                )}
+                  )
+                )
+              ) : (
+                <p>No gallery available</p>
+              )}
+            </p>
+            <div className={styles.propertyDetails}>
+              <Link href={`/properties/${property.sys.id}`}>
+                <h3 className={styles.headline}>{property.fields.address}</h3>
+              </Link>
+              <p className={styles.paragraph}>
+                {property.fields.neighbourhood}
               </p>
 
-              <p
-                className={styles.paragraph}
-              >{`${property.fields.propertyType}`}</p>
-
+              <p className={styles.paragraph}>{property.fields.propertyType}</p>
+              <hr className={styles.hr}></hr>
               <p className={styles.paragraph}>
                 <strong>
                   {property.fields.propertyType === "Rental"
@@ -79,8 +96,12 @@ const Properties = async () => {
                   {property.fields.price}
                 </strong>
               </p>
+              <p className={styles.paragraph}>
+                Beds: {property.fields.beds} | Baths: {property.fields.baths}
+              </p>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
